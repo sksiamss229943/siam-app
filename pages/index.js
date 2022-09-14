ac1c7152a573abca09ecb29a13eb25dffae62c74
sksiamss229943@gmail.com <sksiamss229943@gmail.com>
2022-09-14 19:18:26 +0600

    Full blog page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
-
-const Home = () => {
-  return (
-    <h1>Home Page</h1>
-  )
+import axios from "axios";
+import React from "react";
+import Blog from "../components/Blog";
+export async function getStaticProps() {
+  let res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  let data = res.data;
+  return {
+    props: {
+      data,
+    },
+  };
 }
 
-export default Home;
+function HomePage({ data }) {
+  return (
+    <>
+      <div className="container px-5 pb-[5em] m-0 mx-auto">
+        <h1 className="text-center my-7 font-bold text-5xl">Siam Blogs</h1>
+        <hr className="mb-7" />
+        <div className="blogs space-y-3">
+          {data.map((e) => (
+            <>
+              <Blog key={e.id} id={e.id} title={e.title} body={e.body} />
+            </>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
